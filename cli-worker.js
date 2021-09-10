b62c640f9037818d299e8ac6ae13b656e5f7a30e
KatieFrogs <23621460+KatieFrogs@users.noreply.github.com>
2021-09-10 04:48:34 +0300

    Add page style, use worker
    - Add log output to the page
    - Page no longer freezes during conversion

---
+++ cli-worker.js
@@ -0,0 +1,121 @@
+async function messageEvent(data){
+	var content = data.content
+	var error
+	try{
+		switch(data.subject){
+			case "convertFile":
+				content = await convertFile(content)
+				break
+			default:
+				error = new Error("Unknown message subject")
+				break
+		}
+	}catch(e){
+		error = e
+	}
+	return postMessage({
+		subject: data.subject,
+		error: error,
+		content: content
+	})
+}
+
+async function convertFile(file){
+	var inputFilename = file.name
+	var outputFilename = inputFilename + ".wav"
+	var data = new Uint8Array(await file.arrayBuffer())
+	var stream = FS.open(inputFilename, "w+")
+	FS.write(stream, data, 0, data.length, 0)
+	FS.close(stream)
+	stdoutBuffer = ""
+	stderrBuffer = ""
+	try{
+		callMain([inputFilename])
+	}catch(e){
+		return {
+			error: {
+				type: "wasm",
+				stack: cleanError(e)
+			},
+			stdout: stdoutCopy(),
+			stderr: stderrCopy()
+		}
+	}finally{
+		FS.unlink(inputFilename)
+	}
+	try{
+		var wav = FS.open(outputFilename, "r")
+	}catch(e){
+		return {
+			error: {
+				type: "unsupported",
+				stack: cleanError(e)
+			},
+			stdout: stdoutCopy(),
+			stderr: stderrCopy()
+		}
+	}
+	var wavdata = new Uint8Array(wav.node.usedBytes)
+	FS.read(wav, wavdata, 0, wav.node.usedBytes, 0)
+	FS.close(wav)
+	FS.unlink(outputFilename)
+	return {
+		inputFilename: inputFilename,
+		outputFilename: outputFilename,
+		url: URL.createObjectURL(new Blob([wavdata], {
+			type: "audio/x-wav"
+		})),
+		stdout: stdoutCopy(),
+		stderr: stderrCopy()
+	}
+}
+
+async function loadCli(){
+	try{
+		(0, eval)(await (await fetch("vgmstream-cli.js")).text())
+	}catch(e){
+		return postMessage({
+			subject: "load",
+			error: "Error loading vgmstream-cli.js"
+		})
+	}
+	await new Promise(resolve => {
+		Module["onRuntimeInitialized"] = resolve
+	})
+	return postMessage({
+		subject: "load"
+	})
+}
+
+function cleanError(error){
+	for(var i in error){
+		if(typeof error[i] === "function"){
+			delete error[i]
+		}
+	}
+}
+
+function stdoutCopy(){
+	var stdout = stdoutBuffer
+	stdoutBuffer = ""
+	return stdout
+}
+
+function stderrCopy(){
+	var stderr = stderrBuffer
+	stderrBuffer = ""
+	return stderr
+}
+
+var stdoutBuffer = ""
+var stderrBuffer = ""
+var Module = {
+	preRun: () =>
+		FS.init(undefined, code => {
+			stdoutBuffer += String.fromCharCode(code)
+		}, code => {
+			stderrBuffer += String.fromCharCode(code)
+		})
+}
+addEventListener("message", event => messageEvent(event.data))
+loadCli()
---
+++ player.js
@@ -0,0 +1,155 @@
+var input = document.getElementById("input")
+var selectbtn = document.getElementById("selectbtn")
+var audiobox = document.getElementById("audiobox")
+var filenamebox = document.getElementById("filenamebox")
+var audio = document.getElementById("audio")
+var download = document.getElementById("download")
+var log = document.getElementById("log")
+var fadeoverlay = document.getElementById("fadeoverlay")
+var dlfilename
+
+class WorkerWrapper{
+	constructor(url){
+		this.allEvents = new Map()
+		this.worker = new Worker(url)
+		this.worker.addEventListener("message", event => this.messageEvent(event.data))
+		this.worker.addEventListener("error", event => this.messageEvent({
+			subject: "load",
+			error: "Error loading " + url
+		}))
+		this.on("load").then(() => {
+			this.loaded = true
+		}, error => {
+			alert(error)
+		})
+	}
+	send(subject, content){
+		return this.load().then(() => {
+			return new Promise((resolve, reject) => {
+				this.on(subject).then(resolve, reject)
+				return this.worker.postMessage({
+					subject: subject,
+					content: content
+				})
+			})
+		})
+	}
+	messageEvent(data){
+		var addedType = this.allEvents.get(data.subject)
+		if(addedType){
+			addedType.forEach(callback => {
+				if(data.error){
+					callback.reject(data.error)
+				}else{
+					callback.resolve(data.content)
+				}
+			})
+			this.allEvents.delete(data.subject)
+		}
+	}
+	load(){
+		if(this.loaded){
+			return Promise.resolve(this.worker)
+		}else if(this.loadError){
+			return Promise.reject()
+		}else{
+			return this.on("load")
+		}
+	}
+	on(type){
+		return new Promise((resolve, reject) => {
+			var addedType = this.allEvents.get(type)
+			if(!addedType){
+				addedType = new Set()
+				this.allEvents.set(type, addedType)
+			}
+			addedType.add({
+				resolve: resolve,
+				reject: reject
+			})
+		})
+	}
+}
+var cliWorker = new WorkerWrapper("cli-worker.js")
+
+async function convertFile(file){
+	fade(1, true)
+	try{
+		var response = await cliWorker.send("convertFile", file)
+	}finally{
+		fade(0)
+	}
+	return response
+}
+
+function insertAudio(response){
+	if(response.error){
+		if(response.error.type === "wasm"){
+			alert("Segmentation fault (core dumped)")
+		}else{
+			alert("Could not convert file: " + response.stderr.trim())
+		}
+	}else if(response.url){
+		if(audio.src){
+			URL.revokeObjectURL(audio.src)
+		}
+		audio.src = response.url
+		dlfilename = response.outputFilename
+		filenamebox.innerText = response.inputFilename
+		log.innerText = response.stdout.trim()
+		audiobox.style.display = "block"
+	}
+}
+
+function fade(opacity, modal){
+	fadeoverlay.style.opacity = opacity
+	if(modal){
+		fadeoverlay.style.pointerEvents = "auto"
+		fadeoverlay.style.color = "#fff"
+	}else{
+		fadeoverlay.style.pointerEvents = ""
+		fadeoverlay.style.color = ""
+	}
+}
+
+document.addEventListener("dragover", event => {
+	event.preventDefault()
+	event.dataTransfer.dropEffect = "copy"
+	fade(0.5)
+})
+document.addEventListener("dragleave", () => {
+	fade(0)
+})
+document.addEventListener("drop", event => {
+	fade(0)
+	event.preventDefault()
+	var file = event.dataTransfer.files[0]
+	if(file){
+		convertFile(file).then(insertAudio)
+	}
+})
+input.addEventListener("change", event => {
+	var file = input.files[0]
+	if(file){
+		convertFile(file).then(insertAudio)
+	}
+})
+selectbtn.addEventListener("click", event => {
+	input.click()
+})
+download.addEventListener("click", event => {
+	var link = document.createElement("a")
+	link.href = audio.src
+	if("download" in HTMLAnchorElement.prototype){
+		link.download = dlfilename
+	}else{
+		link.target = "_blank"
+	}
+	link.innerText = "."
+	link.style.opacity = "0"
+	document.body.appendChild(link)
+	setTimeout(() => {
+		link.click()
+		document.body.removeChild(link)
+	})
+})
